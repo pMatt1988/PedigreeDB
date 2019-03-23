d6fc906ab20be15939756e168302ae74be33c29e
vagrant <vagrant@homestead>
2019-03-23 03:34:33 +0000

    a

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Dog;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     //
+
+    public function result(Request $request) {
+        $result = Dog::where('name', "LIKE", "%{$request->input('search')}%");
+        return response()->json($result);
+    }
 }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
+@push('after-styles')
+    {{ style('css/typeaheadjs.css') }}
+@endpush
+
 @section('content')
     <div class="row mb-4">
         <div class="col">
@@ -16,7 +20,7 @@
                         <div class="row">
                             <div class="col-4">
                                 <input type="text" name="search" id="search"
-                                                                   class="form-control">
+                                       class="form-control">
                             </div>
                             <div class="col-2">
                                 <button type="submit" class="form-control">Search</button>
@@ -31,7 +35,17 @@
 @endsection
 
 @push('after-scripts')
+    {{ script("js/typeahead.js" )}}
     <script type="text/javascript">
-        $('#search').typeahead
+        var path = "{{ url('autocomplete') }}";
+        $('#search').typeahead({
+            minLength: 3,
+            highlight: true,
+            source: function (query, process) {
+                return $.get(path, {query: query}, function(data) {
+                    return process(data);
+                });
+            }
+        });
     </script>
-    @endpush
+@endpush
