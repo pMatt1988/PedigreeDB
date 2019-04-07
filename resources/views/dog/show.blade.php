a765d6906e29911821ecb705d1752675ec529d2c
vagrant <vagrant@homestead>
2019-04-07 07:23:03 +0000

    Made Pedigree Image prettier

--- resources/views/dog/show.blade.php
+++ resources/views/dog/show.blade.php
@@ -9,51 +9,56 @@
                     <strong>Dog Name</strong>
                 </div><!--card-header-->
                 <div class="card-body">
-                    @if($dog->image_url != null)
-                        <img src="{{$dog->image_url}}" alt="blah">
-                    @endif
 
                     <div class="row">
-                        <div class="col-2 text-right">Name:</div>
-                        <div class="col">{{ $dog->name }}</div>
-
-                    </div>
-
-                    <div class="row">
-                        <div class="col-2 text-right">Sire:</div>
-
-                        <div class="col">
-                            @if($dog->father())
-                                <a href="/dogs/{{ $dog->father()->id }}">{{ $dog->father()->name }}</a>
-                            @else
-                                N/A
+                        <div class="col-3">
+                            @if($dog->image_url != null)
+                                <img src="{{$dog->image_url}}" alt="blah" class="img-fluid">
                             @endif
                         </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-2 text-right">Dam:</div>
-
                         <div class="col">
-                            @if($dog->mother())
-                                <a href="/dogs/{{ $dog->mother()->id }}">{{ $dog->mother()->name }}</a>
-                            @else
-                                N/A
-                            @endif
-                        </div>
-                    </div>
+                            <div class="row">
+                                <div class="col-2 text-right">Name:</div>
+                                <div class="col">{{ $dog->name }}</div>
 
-                    @if($dog->dob)
-                        <div class="row">
-                            <div class="col-2 text-right">Dob:</div>
-
-                            <div class="col">
-                                {{$dog->dob->format('Y-m-d')}}
                             </div>
+
+                            <div class="row">
+                                <div class="col-2 text-right">Sire:</div>
+
+                                <div class="col">
+                                    @if($dog->father())
+                                        <a href="/dogs/{{ $dog->father()->id }}">{{ $dog->father()->name }}</a>
+                                    @else
+                                        N/A
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-2 text-right">Dam:</div>
+
+                                <div class="col">
+                                    @if($dog->mother())
+                                        <a href="/dogs/{{ $dog->mother()->id }}">{{ $dog->mother()->name }}</a>
+                                    @else
+                                        N/A
+                                    @endif
+                                </div>
+                            </div>
+
+                            @if($dog->dob)
+                                <div class="row">
+                                    <div class="col-2 text-right">Dob:</div>
+
+                                    <div class="col">
+                                        {{$dog->dob->format('Y-m-d')}}
+                                    </div>
+                                </div>
+                            @endif
                         </div>
-                    @endif
 
-
+                    </div>
                 </div><!--card-body-->
             </div><!--card-->
             <br>
